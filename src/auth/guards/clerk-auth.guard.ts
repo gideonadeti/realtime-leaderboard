14f3187dcaaad13cf193a70ac05cb51fb6bb126d
Gideon Adeti <gideonadeti0@gmail.com>
2025-05-15 16:15:33 +0000

    refactor(auth): Improve type safety in ClerkAuthGuard

--- src/auth/guards/clerk-auth.guard.ts
+++ src/auth/guards/clerk-auth.guard.ts
@@ -1,3 +1,4 @@
+import { AuthObject } from '@clerk/express';
 import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
 import { Request } from 'express';
 
@@ -6,8 +7,8 @@ export class ClerkAuthGuard implements CanActivate {
   canActivate(context: ExecutionContext) {
     const request = context
       .switchToHttp()
-      .getRequest<Request & { auth: any }>();
+      .getRequest<Request & { auth: AuthObject }>();
 
-    return request.auth !== undefined;
+    return request.auth.userId !== null;
   }
 }

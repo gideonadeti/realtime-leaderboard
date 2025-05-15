efa285db412076b11326efd6fdced43f6c8116bb
Gideon Adeti <gideonadeti0@gmail.com>
2025-05-15 13:58:17 +0000

    feat(activities): Enhance ActivitiesController with guards and decorators
    Added ClerkAuthGuard for authentication and RolesGuard for admin access
    control in the ActivitiesController. Updated Swagger documentation to
    include the Activities tag for better API organization.

--- src/activities/activities.controller.ts
+++ src/activities/activities.controller.ts
@@ -1,12 +1,32 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  UseGuards,
+} from '@nestjs/common';
+
 import { ActivitiesService } from './activities.service';
 import { CreateActivityDto } from './dto/create-activity.dto';
 import { UpdateActivityDto } from './dto/update-activity.dto';
+import { ClerkAuthGuard } from 'src/auth/guards/clerk-auth.guard';
+import { RolesGuard } from 'src/roles/roles.guard';
+import { UserRole } from 'generated/prisma';
+import { Roles } from 'src/roles/roles.decorator';
 
+@ApiTags('Activities')
+@ApiBearerAuth()
+@UseGuards(ClerkAuthGuard)
 @Controller('activities')
 export class ActivitiesController {
   constructor(private readonly activitiesService: ActivitiesService) {}
 
+  @UseGuards(RolesGuard)
+  @Roles(UserRole.ADMIN)
   @Post()
   create(@Body() createActivityDto: CreateActivityDto) {
     return this.activitiesService.create(createActivityDto);
@@ -22,11 +42,18 @@ export class ActivitiesController {
     return this.activitiesService.findOne(+id);
   }
 
+  @UseGuards(RolesGuard)
+  @Roles(UserRole.ADMIN)
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateActivityDto: UpdateActivityDto) {
+  update(
+    @Param('id') id: string,
+    @Body() updateActivityDto: UpdateActivityDto,
+  ) {
     return this.activitiesService.update(+id, updateActivityDto);
   }
 
+  @UseGuards(RolesGuard)
+  @Roles(UserRole.ADMIN)
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.activitiesService.remove(+id);

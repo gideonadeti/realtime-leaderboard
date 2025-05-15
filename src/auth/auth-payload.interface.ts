9e347af4520bb6905cb24c7258ba78491fbbd72a
Gideon Adeti <gideonadeti0@gmail.com>
2025-05-15 08:04:15 +0000

    feat(auth): implement authentication module with JWT and local strategies
    - Added User and RefreshToken models to Prisma schema for user management.
    - Created AuthModule with necessary services, controllers, and guards.
    - Implemented sign-up and sign-in functionality with password hashing and JWT token generation.
    - Added refresh token functionality with cookie management.
    - Created DTOs for sign-in and sign-up requests.
    - Implemented guards for JWT and local authentication.
    - Added public decorator for public routes.
    - Integrated cookie-parser middleware for handling cookies.
    - Set up global validation pipes for incoming requests.

---
+++ src/auth/auth-payload.interface.ts
@@ -0,0 +1,5 @@
+export interface AuthPayload {
+  email: string;
+  sub: string;
+  jti: string;
+}
---
+++ src/auth/strategies/refresh-jwt.strategy.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@nestjs/common';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { PassportStrategy } from '@nestjs/passport';
+import { Request } from 'express';
+
+import { jwtConstants } from '../jwt.constants';
+
+@Injectable()
+export class RefreshJwtStrategy extends PassportStrategy(
+  Strategy,
+  'refresh-jwt',
+) {
+  constructor() {
+    super({
+      jwtFromRequest: ExtractJwt.fromExtractors([
+        (req: Request) =>
+          (req.cookies as { refreshToken: string }).refreshToken,
+      ]),
+      secretOrKey: jwtConstants.refreshSecret as string,
+    });
+  }
+
+  validate(payload: { sub: string; email: string }) {
+    return { id: payload.sub, email: payload.email };
+  }
+}

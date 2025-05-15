5e5f19c46d7868b4536cef7ab23e81f2ae2b4655
Gideon Adeti <gideonadeti0@gmail.com>
2025-05-15 18:36:10 +0000

    feat(scores): Add CreateScoreDto with validation rules

--- src/scores/dto/create-score.dto.ts
+++ src/scores/dto/create-score.dto.ts
@@ -1,1 +1,19 @@
-export class CreateScoreDto {}
+import { IsInt, IsNotEmpty, IsString } from 'class-validator';
+
+export class CreateScoreDto {
+  /**
+   * Score's value
+   * @example 21
+   */
+  @IsInt()
+  @IsNotEmpty()
+  value: number;
+
+  /**
+   * Score's activity id
+   * @example '34130d3a47a4137caa33e19a'
+   */
+  @IsString()
+  @IsNotEmpty()
+  activityId: string;
+}

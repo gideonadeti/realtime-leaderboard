97c3c1d554cb66b120569d1894ff46e28a568b9c
Gideon Adeti <gideonadeti0@gmail.com>
2025-05-15 15:03:16 +0000

    feat(activities): Add CreateActivityDto with validation

--- src/activities/dto/create-activity.dto.ts
+++ src/activities/dto/create-activity.dto.ts
@@ -1,1 +1,11 @@
-export class CreateActivityDto {}
+import { IsNotEmpty, IsString } from 'class-validator';
+
+export class CreateActivityDto {
+  /**
+   * Activity's name
+   * @example "Running"
+   */
+  @IsString()
+  @IsNotEmpty()
+  name: string;
+}

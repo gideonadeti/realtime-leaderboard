eb3712935320786b3fa50ed4e4343f1ae9ad4a59
Gideon Adeti <gideonadeti0@gmail.com>
2025-05-15 11:43:22 +0000

    feat(activities): Set up activities endpoints

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { MiddlewareConsumer, Module } from '@nestjs/common';
 import { PrismaService } from './prisma/prisma.service';
 import { AuthModule } from './auth/auth.module';
 import { LoggingMiddleware } from './logging/logging.middleware';
+import { ActivitiesModule } from './activities/activities.module';
 
 @Module({
-  imports: [AuthModule],
+  imports: [AuthModule, ActivitiesModule],
   controllers: [],
   providers: [PrismaService],
 })

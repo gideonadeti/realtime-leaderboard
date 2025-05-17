73b4ff830f02894e14b72460d7cb27d2388077eb
Gideon Adeti <gideonadeti0@gmail.com>
2025-05-17 18:34:02 +0000

    refactor(scores): Enhance CreateScoreDto validation rules

--- src/scores/dto/create-score.dto.ts
+++ src/scores/dto/create-score.dto.ts
@@ -1,19 +1,21 @@
-import { IsInt, IsNotEmpty, IsString } from 'class-validator';
+import { IsInt, IsNotEmpty, IsPositive, IsString, Max } from 'class-validator';
 
 export class CreateScoreDto {
   /**
    * Score's value
    * @example 21
    */
-  @IsInt()
   @IsNotEmpty()
+  @IsInt()
+  @IsPositive()
+  @Max(100)
   value: number;
 
   /**
    * Score's activity id
    * @example '34130d3a47a4137caa33e19a'
    */
-  @IsString()
   @IsNotEmpty()
+  @IsString()
   activityId: string;
 }

b3a3dffaa5954f397da390e6538b70892f4602c2
Gideon Adeti <gideonadeti0@gmail.com>
2025-11-19 08:44:56 +0000

    refactor(app): Simplify AppModule by removing ScoresModule, ActivitiesModule, AttachUserMiddleware, and WebhooksController to streamline the codebase

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,22 +5,12 @@ import { JwtService } from '@nestjs/jwt';
 import { PrismaService } from './prisma/prisma.service';
 import { AuthModule } from './auth/auth.module';
 import { LoggingMiddleware } from './logging/logging.middleware';
-import { ActivitiesModule } from './activities/activities.module';
-import { AttachUserMiddleware } from './attach-user/attach-user.middleware';
-import { ScoresModule } from './scores/scores.module';
 import { RedisService } from './redis/redis.service';
 import { LeaderboardGateway } from './leaderboard/leaderboard.gateway';
 import { AuthService } from './auth/auth.service';
-import { WebhooksController } from './webhooks/webhooks.controller';
 
 @Module({
-  imports: [
-    AuthModule,
-    ActivitiesModule,
-    ConfigModule.forRoot({ isGlobal: true }),
-    ScoresModule,
-  ],
-  controllers: [WebhooksController],
+  imports: [AuthModule, ConfigModule.forRoot({ isGlobal: true })],
   providers: [
     PrismaService,
     RedisService,
@@ -31,6 +21,6 @@ import { WebhooksController } from './webhooks/webhooks.controller';
 })
 export class AppModule {
   configure(consumer: MiddlewareConsumer) {
-    consumer.apply(LoggingMiddleware, AttachUserMiddleware).forRoutes('*');
+    consumer.apply(LoggingMiddleware).forRoutes('*');
   }
 }

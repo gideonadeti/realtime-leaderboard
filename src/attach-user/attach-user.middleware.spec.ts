27084c806dcf018146b34fff2c1424441a20e91e
Gideon Adeti <gideonadeti0@gmail.com>
2025-11-19 08:39:28 +0000

    refactor(attach-user): Remove AttachUserMiddleware and associated tests
    Deleted the AttachUserMiddleware and its test file to streamline the codebase and eliminate unused components.

--- src/attach-user/attach-user.middleware.spec.ts
+++
@@ -1,7 +0,0 @@
-import { AttachUserMiddleware } from './attach-user.middleware';
-
-describe('AttachUserMiddleware', () => {
-  it('should be defined', () => {
-    expect(new AttachUserMiddleware()).toBeDefined();
-  });
-});

acf2780444bae3ade9e8b19d5bae1ea8334b7819
Gideon Adeti <gideonadeti0@gmail.com>
2025-11-19 21:50:19 +0000

    feat(dto): Enhance CreateGameDto with duration and outcome fields, including validation rules

--- src/games/dto/create-game.dto.ts
+++ src/games/dto/create-game.dto.ts
@@ -1,1 +1,21 @@
-export class CreateGameDto {}
+import { IsEnum, IsInt, IsNotEmpty, Min } from 'class-validator';
+import { GameOutcome } from '@prisma/client';
+
+export class CreateGameDto {
+  /**
+   * Game duration in seconds
+   * @example 300
+   */
+  @IsInt()
+  @IsNotEmpty()
+  @Min(0)
+  duration: number;
+
+  /**
+   * Game outcome
+   * @example WON
+   */
+  @IsEnum(GameOutcome)
+  @IsNotEmpty()
+  outcome: GameOutcome;
+}

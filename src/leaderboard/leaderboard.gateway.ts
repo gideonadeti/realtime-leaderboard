0fe22630658394ec42d0b732bd43594f42566606
Gideon Adeti <gideonadeti0@gmail.com>
2025-11-20 07:02:08 +0000

    refactor(leaderboard): Simplify LeaderboardGateway by removing unused authentication logic and implementing OnGatewayDisconnect

--- src/leaderboard/leaderboard.gateway.ts
+++ src/leaderboard/leaderboard.gateway.ts
@@ -1,52 +1,30 @@
 import { Logger } from '@nestjs/common';
-import { Server, Socket } from 'socket.io';
 import {
   OnGatewayConnection,
-  OnGatewayInit,
+  OnGatewayDisconnect,
   WebSocketGateway,
   WebSocketServer,
 } from '@nestjs/websockets';
-
-import { AuthPayload } from 'src/auth/auth-payload/auth-payload.interface';
-import { AuthService } from 'src/auth/auth.service';
-// import { wsClerkAuthMiddleware } from 'src/auth/middlewares/ws-clerk-auth.middleware';
+import { Server, Socket } from 'socket.io';
 
 @WebSocketGateway()
-export class LeaderboardGateway implements OnGatewayInit, OnGatewayConnection {
-  constructor(private readonly authService: AuthService) {}
-
-  private userSocketMap = new Map<string, string>();
-
-  private logger = new Logger(LeaderboardGateway.name);
-
+export class LeaderboardGateway
+  implements OnGatewayConnection, OnGatewayDisconnect
+{
   @WebSocketServer()
   server: Server;
 
-  afterInit(server: Server) {
-    server.use((socket: Socket & { user: any }, next) => {
-      // wsClerkAuthMiddleware(this.authService)(socket, next).catch(next);
-    });
+  private readonly logger = new Logger(LeaderboardGateway.name);
 
-    this.logger.log('WebSocket server initialized');
-  }
-
-  handleConnection(client: Socket & { user: AuthPayload }) {
-    const userId = client.user.sub;
-
-    this.userSocketMap.set(userId, client.id);
-
+  handleConnection(client: Socket) {
     this.logger.log(`Client with id ${client.id} connected`);
   }
 
-  handleDisconnect(client: Socket & { user: AuthPayload }) {
-    const userId = client.user.sub;
-
-    this.userSocketMap.delete(userId);
-
+  handleDisconnect(client: Socket) {
     this.logger.log(`Client with id ${client.id} disconnected`);
   }
 
-  emitToClients(event: string, data: any) {
-    this.server.emit(event, data);
+  emitToClients(event: string, payload: any) {
+    this.server.emit(event, payload);
   }
 }

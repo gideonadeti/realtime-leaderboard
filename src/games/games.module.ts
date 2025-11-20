73e3f7b12251db80854c4d9f241e96b3352f4e47
Gideon Adeti <gideonadeti0@gmail.com>
2025-11-20 07:22:48 +0000

    feat(games): Add RedisService and LeaderboardGateway to GamesModule for enhanced leaderboard functionality

--- src/games/games.module.ts
+++ src/games/games.module.ts
@@ -3,9 +3,11 @@ import { Module } from '@nestjs/common';
 import { GamesService } from './games.service';
 import { GamesController } from './games.controller';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { RedisService } from 'src/redis/redis.service';
+import { LeaderboardGateway } from 'src/leaderboard/leaderboard.gateway';
 
 @Module({
   controllers: [GamesController],
-  providers: [GamesService, PrismaService],
+  providers: [GamesService, PrismaService, RedisService, LeaderboardGateway],
 })
 export class GamesModule {}
